b988ebe154684e015c984b90be6d47ed9a031647
DenisKostyukov <kostukov08@gmail.com>
2021-03-30 10:16:28 +0300

    add object

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,11 +1,32 @@
 'use strict';
 
-const number=+prompt("Input number");
-const power=+prompt("Input power");
-let counter=1;
-let result=1;
-while(counter<=power){
-  result*=number;
-  counter++;
+const user = {
+  firstName: 'Test',
+  lastName: 'Testovich',
+  age: 18,
+  isMale: true,
+  sayHello: function () {
+    return `Hello my name is ${this.firstName} ${this.lastName}`;
+  },
+  isAdult: function () {
+    return this.age >= 18;
+  }
 }
-console.log(result);
+//alert(user.sayHello());
+//alert(user.isAdult());
+
+const User = function (firstName, lastName, age, isMale) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.age = age;
+  this.isMale = isMale;
+  this.sayHello = function () {
+    return `Hello my name is ${this.firstName} ${this.lastName}`;
+  };
+  this.isAdult = function () {
+    return this.age >= 18;
+  }
+}
+const secondUser = new User('Test', 'Testovich', 19, true);
+console.log(secondUser.sayHello());
+console.log(secondUser.isAdult());
