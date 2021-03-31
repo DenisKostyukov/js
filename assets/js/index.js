831e186e1f13da44e289ecd20ec3354eb4345447
DenisKostyukov <kostukov08@gmail.com>
2021-03-31 11:42:13 +0300

    add array and prototype example

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,12 +1,24 @@
 'use strict';
 
-function createMultiplicationTable(){
-  const table= {};
-  for(let i=1;i<=10;i++){
-    for(let j=1;j<=10;j++){
-      table[`${i} * ${j} = `]= i*j;
+function MyArrayProto() {
+  this.push = function push() {
+    for (let i = 0; i < arguments.length; i++) {
+      this[this.lenght++] = arguments[i];
     }
+    return this.lenght;
   }
-  return table;
+  this.pop = function pop() {
+    if (this.lenght === 0) return
+    const lastValue = this[this.lenght - 1];
+    delete this[--this.lenght];
+    return lastValue;
+
+  }
 }
-console.log(createMultiplicationTable());
+
+function MyArray() {
+  this.lenght = 0;
+    this.push(...arguments);
+}
+MyArray.prototype = new MyArrayProto();
+const myArr1 = new MyArray();
