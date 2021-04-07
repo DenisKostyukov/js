5f972fa594224cc079dcdf3ef858610b8db38dc0
DenisKostyukov <kostukov08@gmail.com>
2021-04-07 13:17:30 +0300

    test homework

--- assets/js/sandbox.js
+++ assets/js/sandbox.js
@@ -1,12 +1,11 @@
 'use strict';
 
-const str = "some string I need";
-const words = str.split(' ');
-let newString ;
-for (let i = 0; i < words.length; i++) {
-  const letters = words[i].split('');
-  letters[0] = letters[0].toUpperCase();
-  words[i]=letters.join('');
+const capitalize = (string) => {
+  const words = string.split(' ');
+  for (let i = 0; i < words.length; i++) {
+    const letters = words[i].split('');
+    letters[0] = letters[0].toUpperCase();
+    words[i] = letters.join('');
+  }
+  return words.join(' ');
 }
-newString=words.join(' ');
-console.log(newString)
