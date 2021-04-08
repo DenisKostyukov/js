be50e259883e5e899330fb778d079e0035f62f1e
DenisKostyukov <kostukov08@gmail.com>
2021-04-08 09:52:17 +0300

    throw example

--- assets/js/sandbox.js
+++ assets/js/sandbox.js
@@ -1,11 +1,10 @@
 'use strict';
 
-const capitalize = (string) => {
-  const words = string.split(' ');
-  for (let i = 0; i < words.length; i++) {
-    const letters = words[i].split('');
-    letters[0] = letters[0].toUpperCase();
-    words[i] = letters.join('');
-  }
-  return words.join(' ');
-}
+const sum1 =(...args) =>{
+ return args.reduce((accumulator, currentValue)=>{
+   if(!Number.isFinite(currentValue)){
+     throw new TypeError('Wrong argument type')
+   }
+    return accumulator + currentValue;
+  }, 0);
+};
