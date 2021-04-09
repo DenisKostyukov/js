45ee25ce7ef6f093d0b23030d12eb0a96ae8c122
DenisKostyukov <kostukov08@gmail.com>
2021-04-09 10:52:49 +0300

    admin example

--- assets/js/class.js
+++ assets/js/class.js
@@ -1,117 +1,27 @@
-class Worker {
-  /**
-   * 
-   * @param {string} name 
-   * @param {string} surname 
-   * @param {number} dailyRate 
-   * @param {number} days 
-   */
-  constructor(name, surname, dailyRate, days) {
+class User {
+  constructor(name, surname, age, isBanned) {
     this.name = name;
     this.surname = surname;
-    this.dailyRate = dailyRate;
-    this.days = days;
+    this.age = age;
+    this.isBanned = isBanned;
   }
-  set dailyRate(newRate) {
-    if (typeof newRate !== "number") {
-      throw new TypeError("Parameter has unnaproppriate type");
-    }
-    this._dailyRate = newRate;
+  set isBanned(newIsBanned) {
+    this._isBanned = newIsBanned;
   }
-  get dailyRate() {
-    return this._dailyRate;
-  }
-  set days(newDaysCount) {
-    if (newDaysCount < 0 || newDaysCount > 31) {
-      throw new RangeError("Wrong days range");
-    }
-    if (typeof newDaysCount !== "number") {
-      throw new TypeError('Parameters have unnapropriate type');
-    }
-    this._days = newDaysCount;
-  }
-  get days() {
-    return this._days;
-  }
-  get salary() {
-    return this._dailyRate * this.days;
+  get isBanned() {
+    return this._isBanned;
   }
 }
-
-class Fuel {
-  constructor(volume, density) {
-    this.volume = volume;
-    this.density = density;
+class Admin extends User {
+  constructor(name, surname, age) {
+    super(name, surname, age, false);
   }
-  set volume(newVolume){
-    if (typeof newVolume !== "number") {
-      throw new TypeError("Wrong type");
+  ban(user) {
+    if (user instanceof User) {
+     return user.isBanned = !user.isBanned;
     }
-    this._volume=newVolume;
-  }
-  get volume(){
-    return this._volume;
-  }
-  set density(newDensity){
-    if (typeof newDensity !== "number") {
-      throw new TypeError("Wrong type");
-    }
-    this._density=newDensity;
-  }
-  get density(){
-    return this._density;
-  }
-  get getFullWeight() {
-    return this.volume * this.density;
+    throw new Error("Is not a user")
   }
 }
-class Auto {
-  constructor(model, weight, fuel) {
-    this.model = model;
-    this.weight = weight;
-    this.fuel = fuel;
-  }
-  set model(newModel) {
-    if (typeof newModel !== "number") {
-      throw new TypeError("Wrong type");
-    }
-    this._model = newModel;
-  }
-  get model() {
-    return _model;
-  }
-  set weight(newWeight){
-    if (typeof newWeight !== "number") {
-      throw new TypeError("Wrong type");
-    }
-    this._weight=newWeight;
-  }
-  get weight(){
-    return this._weight;
-  }
-  get getFullWeight() {
-    return this.weight + this.fuel.getFullWeight();
-  }
-}
-const benzin = new Fuel(50, 0.9);
-const auto = new Auto(12, 1200, benzin);
-
-class Friend {
-  /**
-   * 
-   * @param {string} name 
-   * @param {number} appleCount 
-   * @param {Friend} friend 
-   */
-  constructor(name, appleCount, friend) {
-    this.name = name;
-    this.appleCount = appleCount;
-    this.friend = friend;
-  }
-  getAllApples() {
-    return this.appleCount + this.friend.appleCount;
-  }
-}
-const friend1 = new Friend('John', 5);
-const friend2 = new Friend('Bob', 10, friend1);
-friend1.friend = friend2;
+const u = new User('Denis', 'Kostyukov', 19, true);
+const a = new Admin('John', 'Smith', 32);
