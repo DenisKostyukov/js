eb0fbc462af697e61c5c828ddca74b63b8160b6d
DenisKostyukov <kostukov08@gmail.com>
2021-04-15 10:31:12 +0300

    add stack

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,5 +4,5 @@ const testArray= new MyArray(1,2,3,4);
 
 const linkedList = new LinkedList(2,3,4,5);
 for(const item of linkedList){
-  console.log(item);
+  //console.log(item);
 }
---
+++ assets/js/stack.js
@@ -0,0 +1,40 @@
+'use strict';
+
+class Stack {
+  constructor(maxSize = 10,...args) {
+    this._maxSize = maxSize;
+    this._size = 0;
+    this.push(...args);
+  }
+  get isEmpty() {
+    return this._size === 0;
+  }
+  get size() {
+    return this._size;
+  }
+  push(...args) {
+    
+    for(const item of args){
+      if (this.size >= this._maxSize) {
+        throw new RangeError('Stack overflow');
+      }
+      this[`_${this.size}`] = item;
+      this._size++;
+    }
+    
+    return this.size;
+  }
+  pop() {
+    if (this.size <= 0) {
+      return;
+    }
+    const lastItem = this[`_${this._size-1}`];
+    delete this[`_${this._size-1}`];
+    this._size--;
+    return lastItem;
+  }
+  pip() {
+    return this[`_${this._size-1}`];
+  }
+}
+const stack = new Stack(5,1,2,3,4);
