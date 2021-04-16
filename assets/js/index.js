d27737eaa5c3f262df0282908fe2f3e484509136
DenisKostyukov <kostukov08@gmail.com>
2021-04-16 11:49:44 +0300

    add vocablurary

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,25 @@
 'use strict';
+const vocablurary = new Map();
+vocablurary.set("cat", "кот");
+vocablurary.set("dog", "собака");
+vocablurary.set("table", "стол");
+vocablurary.set("screen", "экран");
+vocablurary.set("structure", "структура");
+vocablurary.set("count", "считать");
+vocablurary.set("set", "установить");
+vocablurary.set("queue", "очередь");
+vocablurary.set("kernel", "ядро");
+
+const input = 'cat count dog set queue kernel to the table';
+const translate=(str)=>{
+  const lower= str.toLowerCase();
+  const splittedArr= lower.split(' ');
+  const translate= splittedArr.map((word)=>{
+    if(vocablurary.has(word)){
+     return vocablurary.get(word);
+    }
+    return word;
+  })
+  const trStr= translate.join(' ');
+  return trStr;
+}
