970cf8e499235f4bf65902d7c5d0f2f9dc512c93
DenisKostyukov <kostukov08@gmail.com>
2021-04-20 12:07:42 +0300

    add binarySearch

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,34 +1,39 @@
 'use strict';
-const vocablurary = new Map();
-vocablurary.set("cat", "кот");
-vocablurary.set("dog", "собака");
-vocablurary.set("table", "стол");
-vocablurary.set("screen", "экран");
-vocablurary.set("structure", "структура");
-vocablurary.set("count", "считать");
-vocablurary.set("set", "установить");
-vocablurary.set("queue", "очередь");
-vocablurary.set("kernel", "ядро");
-
-const input = 'cat count dog set queue kernel to the table';
-const translate = (str) => {
-  return str.toLowerCase().split(' ').map((word) => {
-    return vocablurary.has(word) ? vocablurary.get(word) : word;
-  }).join(' ');
+const num = [1, 2, 3, 4, 5, 6,7, 8, 9,10,234]
+const linearSearch = (arr, value) => {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === value) {
+      return i;
+    }
+  }
+  return -1;
 }
-const user1 = {
-  id: 1,
-  name: 'John',
-  age: 18,
+const binarySearch = (min=0, max=100) => {
+  let start = min;
+  let end = max;
+  let middle = Math.round((start + end) / 2);
+  let accept = confirm(`${middle} это ваше число?`);
+  while (true) {
+    if (accept) {
+      return middle;
+    }
+    if(confirm(`${middle} больше чем ваше число  ?`)){
+      end = middle;
+      middle= Math.floor((start + end) / 2);
+    }
+    else{
+      start = middle;
+      middle= Math.ceil((start + end) / 2);
+    }
+    accept = confirm(`${middle} это ваше число?`);
+  }
 }
-const user2 = {
-  id: 2,
-  name: 'Jane',
-  age: 20,
+const createMultiplicationTable = (limit) => {
+  const table =[];
+  for(let i =1; i<=limit; i++){
+    for(let j=1;j<=limit;j++){
+      table.push(`${i} * ${j} = ${i*j}`);
+    }
+  }
+  return table;
 }
-const u1msg = ["1234", "4342", "sgsg"];
-const u2msg = ["ssgsg", "sfghfd", "fgkjgk"];
-const msg = new Map();
-msg.set(user1.id, u1msg);
-msg.set(user2.id, u2msg);
-const getUserMsgs = (user) => msg.get(user.id);
